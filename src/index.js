0ee38005cb82790d9ef2add4e55c3a0ba31694a5
vakasgnabi <vakasghulamnabi88@gmail.com>
2020-09-29 16:37:35 +0500

    multiple object in a single state example

--- src/index.js
+++ src/index.js
@@ -1,17 +1,72 @@
-import React from 'react';
+import React,{useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Control=({value,children,onIncrease,onDecrease})=>{
+    return(
+        <div className="control">
+            <button onClick={onDecrease}>-</button>
+            <div>
+                <span className="value">{value}</span>
+                <span className="children">{children}</span>
+            </div>
+            <button onClick={onIncrease}>+</button>
+        </div>
+    )
+}
+
+function AudioControlWithOneObject(){
+    const[{volume,bass,mid,treble},setValues]=useState({
+        volume:53,
+        bass:17,
+        mid:51,
+        treble:32
+    });
+
+    const increase=key=>()=>{
+        setValues(values=>({
+            ...values,
+            [key]:values[key]+1
+        }));
+    }
+
+    const decrease=key=>()=>{
+        setValues(values=>({
+            ...values,
+            [key]:values[key]-1
+        }));
+    }
+
+    return (
+        <div className="audio-controls">
+            <Control
+            value={volume}
+            onIncrease={increase('volume')}
+            onDecrease={decrease('volume')}>
+                Volume
+            </Control>
+            <Control
+            value={bass}
+            onIncrease={increase('bass')}
+            onDecrease={decrease('bass')}>
+                Bass
+            </Control>
+            <Control
+            value={mid}
+            onIncrease={increase('mid')}
+            onDecrease={decrease('mid')}>
+                Mid
+            </Control>
+            <Control
+            value={treble}
+            onIncrease={increase('treble')}
+            onDecrease={decrease('treble')}>
+                treble
+            </Control>
+        </div>
+    )
+}
+
+ReactDOM.render(<AudioControlWithOneObject />,document.querySelector('#root'))
+
